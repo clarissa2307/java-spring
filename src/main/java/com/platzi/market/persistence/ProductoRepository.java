cb26c602f6b6160d4545e497586862cdb71f6ffe
clarissa2307 <marthaleo07@gmail.com>
2021-03-15 22:21:58 -0600

    Query Methods

--- src/main/java/com/platzi/market/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductoRepository.java
@@ -12,6 +12,11 @@ public class ProductoRepository {
     public List<Producto> getAll(){
         return (List<Producto>) productoCrudRepository.findAll();
     }
+    public List<Producto> getByCategoria(int idCategoria){
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
 
-
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCrudRepository.findByCantidadStockLlessThanAndEstado(cantidad);
+    }
 }
