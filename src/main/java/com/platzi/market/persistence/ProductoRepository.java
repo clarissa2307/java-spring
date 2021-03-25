6cd804d15e1fd3b80c652a581f71d8f9b17d4591
clarissa2307 <marthaleo07@gmail.com>
2021-03-24 19:23:35 -0600

    implementar

--- src/main/java/com/platzi/market/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductoRepository.java
@@ -2,9 +2,12 @@ package com.platzi.market.persistence;
 
 import com.platzi.market.persistence.crud.ProductoCrudRepository;
 import com.platzi.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+
+@Repository
 
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
@@ -18,5 +21,16 @@ public class ProductoRepository {
 
     public Optional<List<Producto>> getEscasos(int cantidad){
         return productoCrudRepository.findByCantidadStockLlessThanAndEstado(cantidad);
+    }
+
+    public Optional<Producto> getProduct(int productId) {
+        return productoCrudRepository.findById(productId).map(producto -> mapper.toProduct(producto));
+    }
+    public Producto save(Producto product) {
+        Producto producto = mapper.toProducto(product);
+        return mapper.toProduct(productoCrudRepository.save(producto));
+    }
+    public void delete(int productId){
+        productoCrudRepository.deleteById(productId);
     }
 }
