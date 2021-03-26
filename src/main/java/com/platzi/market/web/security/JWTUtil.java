e81880859bb02eaf466535accdc7dd182850a5e9
clarissa2307 <marthaleo07@gmail.com>
2021-03-26 17:27:14 -0600

    general un JWT

---
+++ src/main/java/com/platzi/market/web/security/JWTUtil.java
@@ -0,0 +1,34 @@
+package com.platzi.market.web.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JWTUtil {
+    private static final String KEY = "platzi";
+
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder().setSubject(userDetails.getUsername()).setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(SignatureAlgorithm.HS256, KEY).compact();
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        return userDetails.getUsername().equals(extractUsername(token)) && !isTokenExpired(token);
+    }
+
+    public String extractUsername(String token) {
+        return getClaims(token).getSubject();
+    }
+
+    public boolean isTokenExpired(String token) {
+        return getClaims(token).getExpiration().before(new Date());
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser().setSigningKey(KEY).parseClaimsJws(token).getBody();
+    }
+}
+
