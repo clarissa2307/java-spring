941b6d1f5b1be3f2657e0bdcc6d9acee8c71f877
clarissa2307 <marthaleo07@gmail.com>
2021-04-19 10:49:08 -0500

    dto

--- src/main/java/com/platzi/market/domain/dto/AuthenticationRequest.java
+++ src/main/java/com/platzi/market/domain/dto/AuthenticationRequest.java
@@ -1,8 +1,8 @@
 package com.platzi.market.domain.dto;
 
 public class AuthenticationRequest {
-    private  String username;
-    private  String password;
+    private String username;
+    private String password;
 
     public String getUsername() {
         return username;
